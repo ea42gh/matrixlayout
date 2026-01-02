b655047dcdfcf612e6c4da8651e9156c0a243151
ea42gh <ea42_github@mail.com>
2026-01-02 15:16:22 -0500

    fixes..

--- tests/test_ge.py
+++ tests/test_ge.py
@@ -1,31 +1,30 @@
 import os
-
 import pytest
 
 from matrixlayout.ge import ge_tex
 
 
-def test_ge_tex_includes_core_sections():
+def test_ge_tex_contains_env_and_format():
     tex = ge_tex(
-        mat_rep="1 & 2 \\ 3 & 4",
+        mat_rep="1 & 0 \\\\ 0 & 1",
         mat_format="cc",
-        mat_options="",
-        preamble="\\def\\Foo{bar}",
-        extension="",
-        codebefore=["% codebefore hook"],
-        submatrix_locs=[("name=SM", "(1-1)(2-2)")],
-        submatrix_names=["% submatrix label hook"],
-        pivot_locs=[(" (1-1) ", "red")],
-        txt_with_locs=[("(0,0)", "hello", "black")],
-        rowechelon_paths=["% path hook"],
-        fig_scale=None,
+        landscape=False,
     )
-    assert "\\begin{NiceArray}" in tex
-    assert "1 & 2" in tex
-    assert "\\SubMatrix" in tex
-    assert "txt_with_locs" not in tex  # ensure rendered, not literal variable name
-    assert "hello" in tex
-    assert "\\node [draw" in tex
+    assert r"\begin{pNiceArray}" in tex  # default delimiter environment
+    assert r"{cc}" in tex
+    assert "1 & 0" in tex
+
+
+def test_ge_tex_submatrix_emits_SubMatrix():
+    tex = ge_tex(
+        mat_rep="1 & 2 \\\\ 3 & 4",
+        mat_format="cc",
+        submatrix_locs=[("name=SM", "(1-1)(2-2)")],
+        submatrix_names=[r"\node at (SM-center) {A};"],
+        landscape=False,
+    )
+    assert r"\SubMatrix[name=SM](1-1)(2-2)" in tex
+    assert r"\node at (SM-center) {A};" in tex
 
 
 @pytest.mark.render
@@ -35,7 +34,10 @@ def test_ge_svg_smoke():
     from matrixlayout.ge import ge_svg
 
     svg = ge_svg(
-        mat_rep="1 & 0 \\ 0 & 1",
+        mat_rep="1 & 2 \\\\ 3 & 4",
         mat_format="cc",
+        submatrix_locs=[("name=SM", "(1-1)(2-2)")],
+        submatrix_names=[r"\node at (SM-center) {A};"],
+        landscape=False,
     )
     assert "<svg" in svg
