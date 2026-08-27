aa1e8b585dbc195b6a22a9685d37dbefa05d7915
ea42gh <ea42_github@mail.com>
2026-08-27 14:03:50 -0400

    Bump package version

--- matrixlayout/__about__.py
+++ matrixlayout/__about__.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2026-present ea42gh <ea42_github@mail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __build__ = "4549693 2026-02-06T16:01:30-05:00"
